2587c4d3fb7061a3508e3693fd5bd786a3a8a696
Will Moore <william@webs.com>
2015-04-09 12:00:53 -0400

    Only hello on moonsharks board

--- js/moonsharks.js
+++ js/moonsharks.js
@@ -1,1 +1,4 @@
-window.console && console.log('welcome moonsharks!')
+
+if (WEBS && WEBS.boards && WEBS.boards.MoonSharks && window.location.href.indexOf('rapidView=' + WEBS.boards.MoonSharks.id) > 0) {
+	window.console && console.log('welcome moonsharks!')
+}
