659180a2db01230df7d5ceec9ddeaf04c67b1a50
Will Moore <william@webs.com>
2015-04-13 23:34:39 -0400

    Do a little magic for the moonshark kanban

--- js/moonsharks.js
+++ js/moonsharks.js
@@ -1,4 +1,22 @@
+if (WEBS) {
+	WEBS.currentBoardDeferred.then(function() {
+		if (WEBS.currentBoard.moonSharkBoard) {
+			var moonSharkId = WEBS.boards.MoonSharks.id;
+			var mmonSharkKanbanId = WEBS.boards.MoonSharksKanban.id;
+			if (window.location.href.indexOf('rapidView=' + moonSharkId) > 0) {
+				window.console && console.log('welcome moonsharks!');
+			}
 
-if (WEBS && WEBS.boards && WEBS.boards.MoonSharks && window.location.href.indexOf('rapidView=' + WEBS.boards.MoonSharks.id) > 0) {
-	window.console && console.log('welcome moonsharks!')
+			if (window.location.href.indexOf('rapidView=' + mmonSharkKanbanId) > 0) {
+				window.console && console.log('welcome moonsharks Kanban!');
+				setTimeout(function() {
+					var elem = jQuery('#plan-toggle');
+					elem.removeAttr('disabled');
+					elem.attr('href', '/secure/RapidBoard.jspa?rapidView='+moonSharkId+'&view=planning');
+					elem.attr('data-tooltip', 'Go to the Kanban backlog');
+					elem.off('click');
+				}, 500);
+			}
+		}
+	});
 }
