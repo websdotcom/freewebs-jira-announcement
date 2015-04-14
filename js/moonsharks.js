bf0f967a78bc689cd34d6c1bc6daf44ead246f16
Will Moore <william@webs.com>
2015-04-14 00:25:18 -0400

    Do a little more magic for the backlog

--- js/moonsharks.js
+++ js/moonsharks.js
@@ -2,12 +2,24 @@ if (WEBS) {
 	WEBS.currentBoardDeferred.then(function() {
 		if (WEBS.currentBoard.moonSharkBoard) {
 			var moonSharkId = WEBS.boards.MoonSharks.id;
-			var mmonSharkKanbanId = WEBS.boards.MoonSharksKanban.id;
+			var moonSharkKanbanId = WEBS.boards.MoonSharksKanban.id;
 			if (window.location.href.indexOf('rapidView=' + moonSharkId) > 0) {
 				window.console && console.log('welcome moonsharks!');
+				setTimeout(function() {
+					var elem = jQuery('#work-toggle');
+					elem.attr('href', '/secure/RapidBoard.jspa?rapidView='+moonSharkKanbanId);
+					elem.attr('data-tooltip', 'Kanban Board');
+					elem.off('click');
+					elem.on('click', function(e) {
+						e.preventDefault();
+						// necessary because jira seems to randomly rewrite this URL
+						window.location.href='/secure/RapidBoard.jspa?rapidView='+moonSharkKanbanId;
+					});
+					elem.text('Kanban board');
+				}, 500);
 			}
 
-			if (window.location.href.indexOf('rapidView=' + mmonSharkKanbanId) > 0) {
+			if (window.location.href.indexOf('rapidView=' + moonSharkKanbanId) > 0) {
 				window.console && console.log('welcome moonsharks Kanban!');
 				setTimeout(function() {
 					var elem = jQuery('#plan-toggle');
