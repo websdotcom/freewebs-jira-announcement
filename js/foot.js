5a1dc2d350e25a14d1413768e7b5280d0b091f13
Jack Anderson <jack@webs.com>
2016-02-25 13:56:05 -0500

    run card overlap logic on window resize

--- js/foot.js
+++ js/foot.js
@@ -1,26 +1,29 @@
 setTimeout(function() {
 	if (USER && USER.username === 'tools' && WEBS && WEBS.boards && WEBS.boards.Architects && window.location.href.indexOf('rapidView=' + WEBS.boards.Architects.id) > 0) {
-		jQuery().ready(function() {
-			var cols = jQuery('[data-swimlane-id="117"] + .ghx-columns .ghx-column');
-			var viewportHeight = jQuery(window).height();
-			cols.each(function(i) {
-				var col = jQuery(this);
-				var visibleHeight = viewportHeight - col[0].getBoundingClientRect().top;
-				var issues = col.find('.ghx-issue');
-				var height = issues.length ? issues[0].getBoundingClientRect().height : 0;
-				var issuesHeight = issues.length * height;
-				var hiddenHeight = issuesHeight - visibleHeight;
-				if (hiddenHeight > (height - 50)) {
-					var perIssueAdjustment = hiddenHeight / issues.length;
-					issues.each(function(i) {
-						jQuery(this).css({
-							position: 'relative',
-							top: '-' + (perIssueAdjustment * i) + 'px'
-						});
-					});
-				}
-			});
-		});
+		var overlapCards = function() {
+      var cols = jQuery('[data-swimlane-id="117"] + .ghx-columns .ghx-column');
+      var viewportHeight = jQuery(window).height();
+      cols.each(function(i) {
+        var col = jQuery(this);
+        var visibleHeight = viewportHeight - col[0].getBoundingClientRect().top;
+        var issues = col.find('.ghx-issue');
+        var height = issues.length ? issues[0].getBoundingClientRect().height : 0;
+        var issuesHeight = issues.length * height;
+        var hiddenHeight = issuesHeight - visibleHeight;
+        if (hiddenHeight > (height - 50)) {
+          var perIssueAdjustment = hiddenHeight / issues.length;
+          issues.each(function(i) {
+            jQuery(this).css({
+              position: 'relative',
+              top: '-' + (perIssueAdjustment * i) + 'px'
+            });
+          });
+        }
+      });
+    };
+
+    jQuery().ready(overlapCards);
+    window.addEventListener('resize', overlapCards);
 	}
 }, 1000);
 
