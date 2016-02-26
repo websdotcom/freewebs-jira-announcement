f5ecf883a5db73754da93dbea45240297b6ac52a
Jack Anderson <jack@webs.com>
2016-02-26 08:46:05 -0500

    more overlap fixes

--- js/foot.js
+++ js/foot.js
@@ -1,18 +1,43 @@
 setTimeout(function() {
 	if (USER && USER.username === 'tools' && WEBS && WEBS.boards && WEBS.boards.Architects && window.location.href.indexOf('rapidView=' + WEBS.boards.Architects.id) > 0) {
-		var overlapCards = function() {
+		function debounce(func, wait, immediate) {
+      var timeout;
+      return function() {
+        var context = this, args = arguments;
+        var later = function() {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+      };
+    };
+
+    var overlapCards = debounce(function() {
+      console.log('fitting cards in viewport');
+
       var cols = jQuery('[data-swimlane-id="117"] + .ghx-columns .ghx-column');
       var viewportHeight = jQuery(window).height();
       cols.each(function(i) {
         var col = jQuery(this);
-        var visibleHeight = viewportHeight - col[0].getBoundingClientRect().top;
         var issues = col.find('.ghx-issue');
-        var height = issues.length ? issues[0].getBoundingClientRect().height : 0;
-        var issuesHeight = issues[issues.length - 1].getBoundingClientRect().bottom - col[0].getBoundingClientRect().top;
+
+        if (issues.length === 0) {
+          return;
+        }
+
+        var colTop = col[0].getBoundingClientRect().top;
+        var visibleHeight = viewportHeight - colTop;
+        var issuesHeight = jQuery.makeArray(issues).reduce(function(result, iss, i) {
+          return result + issues.get(i).getBoundingClientRect().height;
+        }, 0);
         var hiddenHeight = issuesHeight - visibleHeight;
         if (hiddenHeight > 0) {
+          console.log('collapsing column ' + i);
           var perIssueAdjustment = visibleHeight / issues.length;
-          perIssueAdjustment -= (issues[issues.length - 1].getBoundingClientRect().height - perIssueAdjustment) / issues.length - 1;
+          perIssueAdjustment -= (issues.get(-1).getBoundingClientRect().height - perIssueAdjustment) / issues.length - 1;
           var totalAdjustments = 0;
           issues.each(function(i) {
             if (i === 0) { return; }
@@ -23,14 +48,16 @@ setTimeout(function() {
             });
           });
         } else {
+          console.log('resetting column ' + i);
           issues.each(function(i) {
             jQuery(this).css({
-              position: 'static'
+              position: 'relative',
+              top: 'auto'
             });
           });
         }
       });
-    };
+    }, 500);
 
     jQuery().ready(overlapCards);
     window.addEventListener('resize', overlapCards);
